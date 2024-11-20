37b2f844cbef2f7548b66744e0a3df1114412d78
Aldwin Rich R. Villanueva <Aldwinrich90@gmail.com>
2024-11-20 23:48:55 +0800

    added table

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
+    <h1>{{ Title }}</h1>
+    <p>{{Description}}</p>
+    <table>
+      <tr>
+      <th >Column 1</th>
+      <th>Column 2</th>
+      <th>Column 3</th>
+      </tr>
+      <tr>
+        <th >Column 1</th>
+        <th>Column 2</th>
+        <th>Column 3</th>
+        <th><p>{{ Date.created }}</p> <button></button></th>
+      </tr>
 
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  setup () {
+  data() {
+    return {
+      
+    }
+  },props: []
+  ,
+  methods: {
     
-
-    return {}
-  }
+  },
 }
 </script>
 
