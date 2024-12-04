c62c489fdb23f2c72e4a5633277f225ce26ae3f6
Aldwin Rich R. Villanueva <Aldwinrich90@gmail.com>
2024-12-05 00:59:33 +0800

    fixed tailwind and added minor design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*/public/{html,js}",
+    './public/index.html',
+  ],
+  
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [],  
 }
