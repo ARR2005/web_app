e875caa3ca357a392f66bd7263596a6b5bed4746
Aldwin Rich R. Villanueva <Aldwinrich90@gmail.com>
2024-12-07 02:11:18 +0800

    added cdn tailwind -output.css showing as mime file

--- assets/index.js
+++ assets/index.js
@@ -6,22 +6,26 @@ const itemRoutes = require('./Route');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(cors()); 
 
-// Middleware to serve static files (e.g., HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'src')));
-
+// Serve static files from public and src
+app.use(express.static(path.join(__dirname, '../public'))); // Serve static files from public
+app.use(express.static(path.join(__dirname, '../src'))); 
 // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // Use item routes
 app.use(itemRoutes);
 
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+  console.log(`Serving static files from: ${path.join(__dirname, '../public')} and ${path.join(__dirname, '../src')}`);
 });
